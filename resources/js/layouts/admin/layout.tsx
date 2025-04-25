df97c7e13cb61cd2190cc3e2e47e4a0921f3b7c0
Rama Devantara <runesong71@gmail.com>
2025-04-25 22:58:10 +0800

    light mode dark mode buat warna tulisan

--- resources/js/layouts/admin/layout.tsx
+++ resources/js/layouts/admin/layout.tsx
@@ -16,8 +16,16 @@ type AuthenticatedLayoutProps = {
 export default function AuthenticatedAdminLayout({ children, user, headerTitle, title }: AuthenticatedLayoutProps) {
     const { url } = usePage();
     const [activeItem, setActiveItem] = useState('');
+    const [theme, setTheme] = useState('dark');
 
     useEffect(() => {
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setTheme(prefersDarkMode ? 'dark' : 'light');
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        });
+
         if (url.includes('dashboard')) {
             setActiveItem('dashboard');
         } else if (url.includes('admins')) {
@@ -31,12 +39,12 @@ export default function AuthenticatedAdminLayout({ children, user, headerTitle, 
         <>
             <Head title={title} />
             <SidebarProvider>
-                <div className="flex h-screen w-full">
+                <div className={`flex h-screen w-full ${theme === 'light' ? 'text-black' : 'text-white-400'}`}>
                     <SidebarNav activeItem={activeItem} setActiveItem={setActiveItem} user={user} />
                     <SidebarInset>
                         <div className="flex h-full flex-col">
                             <DashboardHeader user={user} headerTitle={headerTitle} />
-                            <main className="flex-1 overflow-auto p-4 text-yellow-500 lg:p-6">{children}</main>
+                            <main className="flex-1 overflow-auto p-4 lg:p-6">{children}</main>
                         </div>
                     </SidebarInset>
                 </div>
