dba1c22cd486a3168a176c6fb52b7bd753d0a96e
Lucien Purnawarman <lucienthewizz@gmail.com>
2025-05-07 04:41:38 +0800

    ADD: Middleware Custom Page 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,13 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\NotFoundMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -21,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            NotFoundMiddleware::class,
         ]);
 
 
@@ -38,5 +42,14 @@ return Application::configure(basePath: dirname(__DIR__))
         // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->wantsJson()) {
+                return response()->json(['message' => 'Not Found'], 404);
+            }
+            
+            return Inertia::render('errors/NotFound', [
+                'status' => 404,
+                'message' => 'Halaman yang Anda cari tidak ditemukan.'
+            ])->toResponse($request);
+        });
     })->create();
